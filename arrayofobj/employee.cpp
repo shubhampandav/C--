bc8a01232a973cdbbc67bd3cf9be38e871b49b60
shubham1479 <67508771+shubham1479@users.noreply.github.com>
2021-12-30 23:30:55 +0530

    commit

--- arrayofobj/employee.cpp
+++ arrayofobj/employee.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
-#include<limits>
 using namespace std;
 class Employee
 {
-    string emp_name;
+    string emp_name,emp_dept;
     int emp_id;
     int long long emp_mobno;
     double emp_salary;
     public:
-    void getInfo(string name, int id, int long long mobno, double salary)
+    void getInfo(string name, string dept,int id, int long long mobno, double salary)
     {
+        emp_dept = dept;
         emp_name = name;
         emp_id = id;
         emp_mobno = mobno;
@@ -19,6 +19,7 @@ class Employee
     {
         cout<<"Employee Name = "<<emp_name<<endl;
         cout<<"Employee ID = "<<emp_id<<endl;
+        cout<<"Employee Department = "<<emp_dept<<endl;
         cout<<"Employee Moblie No = "<<emp_mobno<<endl;
         cout<<"Employee Salary  = "<<emp_salary<<endl;
 
@@ -30,36 +31,37 @@ class Employee
 int main()
 {
     int id;
-    string name;
+    string name,dept;
     int long long mobno;
     double salary;
     Employee e[10]; // crating array of objects
 
-    for (int i = 1; i <=2; i++)
+    for (int i = 1; i <=5; i++)
     {
-       cout<<"Enter the name"<<endl;
+       cout<<"Enter the name:"<<endl;
         getline(cin,name); // taking name 
-        cout<<"Enter Id = "<<endl;
+        cout<<"Enter Id : "<<endl;
         cin>>id;
-
-        cout<<"Enter Employee mobile No = "<<endl;
-        cin>>mobno;
-        
-
-        cout<<"Enter salary of Employee ="<<endl;
-        cin>>salary;
-       e[i].getInfo(name,id,mobno,salary);
         cin.ignore();  // this is for to ignore new line 
-
-
-
+        cout<<"Enter the department of Employee: "<<endl;
+        getline(cin,dept);
+        cout<<"Enter Employee mobile No: "<<endl;
+        cin>>mobno;
+        cout<<"Enter salary of Employee:"<<endl;
+        cin>>salary;
+        e[i].getInfo(name,dept,id,mobno,salary);
+        cin.ignore();  // this is for to ignore new line 
     }
     cout<<"The Employee information is: "<<endl;
 
-    for (int  i = 1; i <=2; i++)
+    for (int  i = 1; i <=5; i++)
     {
         e[i].putInfo();
     }
     
     return 0;   
 }
+        
+
+
+
